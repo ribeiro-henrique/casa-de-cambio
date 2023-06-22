ccfeecb6d6c7e79f85238aae0b5886bab489b288
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-06-22 10:56:46 -0300

    feat: created inputs

---
+++ casa-de-cambio/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>
+        Casa de Câmbio
+      </h1>
+      <input
+      placeholder="BRL"
+      className="input"
+      name="text"
+      type="text"
+      />
+      <button>
+        SEARCH
+      </button>
+    </div>
+  )
+}
+
+export default App
