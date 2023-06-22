c35c53b61d8a3fa6de93297d775369b99dc84f5c
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-06-22 14:01:04 -0300

    feat: api

--- casa-de-cambio/src/App.jsx
+++ casa-de-cambio/src/App.jsx
@@ -1,7 +1,30 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [search, setSearch] = useState([]);
+  const [coins, setCoins] = useState([]);
+  console.log(coins);
+  
+
+  const handleChange = ({ target }) => {
+    setSearch(target.value);
+  };
+
+  const handleSearch = () => {
+    const endPoint = search;
+    const url = `https://api.exchangerate.host/latest?base=${endPoint}`;
+
+    let rates;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        rates = data.rates
+        const coinArr = Object.entries(rates);
+        setCoins(coinArr);
+      })
+  }
 
   return (
     <div>
@@ -13,10 +36,35 @@ function App() {
       className="input"
       name="text"
       type="text"
+      onChange={handleChange}
       />
-      <button>
+      <button
+      onClick={handleSearch}
+      >
         SEARCH
       </button>
+      <h2>
+        Valores Referentes a 1 {search}
+      </h2>
+      <table>
+        <tbody>
+          {
+            coins.length > 0 ?
+            coins.map((e, index) => (
+              <tr key={index}>
+                <td>
+                  {e[0]}
+                  --
+                  {e[1]}
+                </td>
+              </tr>
+            ))
+            : <span>
+              Sua Pesquisa
+            </span>
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
